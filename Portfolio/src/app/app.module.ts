421c69bac21f7b4f5634115aafa7bd1d51636279
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2022-05-02 10:47:21 -0400

    created content folder and coding languages page component

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './homepage/homepage.component';
+import { CodingLanguagesComponent } from './content/coding-languages/coding-languages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    CodingLanguagesComponent,
   ],
   imports: [
     BrowserModule,
