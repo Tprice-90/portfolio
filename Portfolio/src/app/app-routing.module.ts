14112189f0bea9d2c456d8431c7196e6a01f0bed
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2022-05-28 19:23:02 -0400

    Created Projects Component, updated about component h2
    Projects component is empty at the moment, will update soon. Also updated mobile routerLinks

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: "about",
     component: AboutComponent
+  },
+  {
+    path: "projects",
+    component: ProjectsComponent
   }
 ];
 
